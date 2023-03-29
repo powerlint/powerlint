8195a7afd2baefa229c1d6588c7b5d5210a3549d
Luke Carr <me+oss@carr.sh>
2023-03-29 18:42:35 +0000

    📝 Added missing jsdoc documentation

--- packages/pbix/src/index.ts
+++ packages/pbix/src/index.ts
@@ -1,27 +1,72 @@
 import AdmZip from 'adm-zip'
-import { parseSection, Section } from './parsers/section'
+import { parseSection, Page, Section } from './parsers/section'
+export type { Page } from './parsers/section'
+export type { Visual } from './parsers/visual-container'
 
+/**
+ * Parses a Power BI report file (.pbix) and extracts metadata and report structure into
+ * developer-friendly (fully typed) objects.
+ * 
+ * @param pathOrBuffer The file path (or buffer of the file contents if already read from the filesystem).
+ * @returns The parsed Power BI report metadata.
+ */
 export async function parseReport(pathOrBuffer: string | Buffer) {
+    // Instantiate zip archive from report file
     const archive = new AdmZip(pathOrBuffer)
     const entries = archive.getEntries()
 
+    // Attempt to fetch report layout file from zip archive
     const layoutEntry = entries.find(({ entryName }) => entryName === 'Report/Layout')
     if (typeof layoutEntry === 'undefined') throw new Error('Provided .pbix file does not contain a report layout file!')
 
+    // Extract raw layout file's data from zip archive
     const rawLayout = await new Promise<Buffer>((resolve, reject) => layoutEntry.getDataAsync((data, err) => {
         if (err) return reject(err)
         resolve(data)
     }))
 
-    const layout: ReportLayout = JSON.parse(rawLayout.toString('utf-8').replace(/[\u0000-\u0019]+/g, ''))
+    // Remove invisible characters from layout file (otherwise JSON parsing fails!)
+    const { theme, sections }: ReportLayout = JSON.parse(rawLayout.toString('utf-8').replace(/[\u0000-\u0019]+/g, ''))
     
+    // Parse pages from report layout
+    const pages = sections.map(parseSection)
+
     return {
-        pages: layout.sections.map(parseSection),
+        theme,
+        pages,
     }
 }
 
+/**
+ * Represents a Power BI report.
+ * 
+ * This type is the parsed type returned by this library. For the raw type within a .pbix file,
+ * see {@link ReportLayout}.
+ */
+export type Report = {
+    /**
+     * The report's configured theme/appearance.
+     */
+    theme: string
+    /**
+     * The pages within the report.
+     */
+    pages: Page[]
+}
+
+/**
+ * Represents a Power BI report.
+ * 
+ * This type is the raw type stored within a .pbix file. For the parsed type, see {@link Report}.
+ */
 type ReportLayout = {
     id: number
+    /**
+     * The name of the report's theme.
+     */
     theme: string
+    /**
+     * The pages within the report (referred to in the .pbix file format as "sections").
+     */
     sections: Section[]
 }
--- packages/pbix/src/parsers/section.ts
+++ packages/pbix/src/parsers/section.ts
@@ -1,5 +1,12 @@
 import { parseVisualContainer, Visual, VisualContainer } from './visual-container'
 
+/**
+ * Parses a raw {@link Section} object (extracted from a .pbix file) into a more
+ * developer-friendly type ({@link Page}).
+ * 
+ * @param rawSection The raw page (referred to as a "section" in the .pbix file format) to parse.
+ * @returns The parsed report page.
+ */
 export function parseSection(rawSection: Section): Page {
     const { name: id, displayName: name, config: rawConfig, filters: rawFilters, visualContainers, ...section } = rawSection
 
@@ -12,33 +19,102 @@ export function parseSection(rawSection: Section): Page {
         config,
         filters,
         visuals: visualContainers.map(parseVisualContainer),
+        // Calculate `hidden` property from the `visiblility` property of the page config
         hidden: config['visibility'] === 1,
         ...section,
     }
 }
 
+/**
+ * Represents a page within a Power BI report.
+ * 
+ * This type is the parsed type returned by this library. For the raw type within a .pbix file,
+ * see {@link Section}.
+ */
 export type Page = {
+    /**
+     * The unique identifier for the page.
+     */
     id: string
+    /**
+     * The "display name" of the page, displayed to report consumers.
+     */
     name: string
     displayOption: number
+    /**
+     * Page-specific filters currently applied.
+     */
     filters: unknown
+    /**
+     * The page's configuration data.
+     */
     config: unknown
+    /**
+     * Whether the page is hidden (true) or not (false) from report consumers.
+     */
     hidden: boolean
-    height: number
+    /**
+     * The width of the page (in pixels).
+     */
     width: number
+    /**
+     * The height of the page (in pixels).
+     */
+    height: number
+    /**
+     * The index of the page. This determines where the page appears in the Power BI report's
+     * page navigation menu. A lower number indicates that the page appears first.
+     */
     ordinal: number
+    /**
+     * The page's visuals.
+     */
     visuals: Visual[]
 }
 
+/**
+ * Represents a page within a Power BI report.
+ * 
+ * This type is the raw type stored within a .pbix file. For the parsed type, see {@link Page}.
+ */
 export type Section = {
+    /**
+     * The unique identifier for the page.
+     * 
+     * During parsing, this is remapped to the `id` property of the {@link Page} type.
+     */
     name: string
+    /**
+     * The "display name" of the page, displayed to report consumers.
+     * 
+     * During parsing, this is remapped to the `name` property of the {@link Page} type.
+     */
     displayName: string
     displayOption: number
-    height: number
+    /**
+     * The width of the page (in pixels).
+     */
     width: number
+    /**
+     * The height of the page (in pixels).
+     */
+    height: number
+    /**
+     * The page's configuration data.
+     */
     config: unknown
+    /**
+     * Page-specific filters currently applied.
+     */
     filters: unknown
+    /**
+     * The index of the page. This determines where the page appears in the Power BI report's
+     * page navigation menu. A lower number indicates that the page appears first.
+     */
     ordinal: number
+    /**
+     * The page's visuals.
+     */
     visualContainers: VisualContainer[]
 }
 
