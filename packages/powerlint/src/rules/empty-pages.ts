bae8eaeb86234299e4670b10ab20ca62223ffed9
Luke Carr <me+oss@carr.sh>
2023-04-01 18:01:40 +0100

    ✨ Created initial `powerlint` CLI codebase (#7)
    * ✨ Created initial `powerlint` CLI codebase
    
    * 📝 Added example screenshot to README
    
    Signed-off-by: Luke Carr <me+oss@carr.sh>
    
    ---------
    
    Signed-off-by: Luke Carr <me+oss@carr.sh>

---
+++ packages/powerlint/src/rules/empty-pages.ts
@@ -0,0 +1,11 @@
+import { Report } from 'pbix'
+import { PageError, Rule, RuleMetadata } from '../rule'
+
+@RuleMetadata({ name: 'empty-pages', description: 'Report pages must have at least one visual' })
+export class EmptyPagesRule implements Rule {
+    apply(report: Report) {
+        return report.pages
+            .filter(({ visuals }) => visuals.length === 0)
+            .map(({ name }) => new PageError('Page must have at least one visual', name))
+    }
+}
