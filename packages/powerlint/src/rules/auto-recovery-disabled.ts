3479e49db47b1ca359c51a3343108cfbd73f0943
Luke Carr <me+oss@carr.sh>
2023-04-01 21:46:26 +0000

    ✏️ Applied consistent grmamar to rule messages

--- packages/powerlint/src/rules/auto-recovery-disabled.ts
+++ packages/powerlint/src/rules/auto-recovery-disabled.ts
@@ -4,6 +4,6 @@ import { LintingError, Rule, RuleMetadata } from '../rule'
 @RuleMetadata({ name: 'auto-recovery-disabled', description: 'Ensure that report auto recovery is enabled' })
 export class AutoRecoveryDisabledRule implements Rule {
     apply(report: Report) {
-        return report.settings.report?.autoRecovery === false ? [new LintingError('Reports must have auto recovery enabled')] : []
+        return report.settings.report?.autoRecovery === false ? [new LintingError('Report must have auto recovery enabled')] : []
     }
 }
